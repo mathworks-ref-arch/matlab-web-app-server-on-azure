8bb8cc3e0bc5bba0d1050c9f476937fec7c362fe
Hema Gullapalli <62145510+hgullapa@users.noreply.github.com>
2025-05-22 16:45:03 -0400

    Update deploy.py to add '-' in subnet name with uuid

--- healthcheck/testtools/deploy.py
+++ healthcheck/testtools/deploy.py
@@ -103,7 +103,7 @@ def create_vnet(credentials,
     for i in range(1, len(subnets_cidr) + 1):
 
          # Create Subnet
-         subnet_name = "subnet_" + str(i) + "_" + str(uuid.uuid4())
+         subnet_name = "subnet_" + str(i) + "-" + str(uuid.uuid4())
          async_subnet_creation = network_client.subnets.create_or_update(
                                                resource_name_vnet,
                                                vnet_name,
